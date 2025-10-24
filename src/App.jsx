6181a7d7e01a638a34c681044f576e7cd83deb92
nandiitha <mnanditha71@gmail.com>
2025-10-24 13:38:42 +0530

    setup React + Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import './index.css';
+
+export default function App() {
+  return (
+    <div class="min-h-screen flex items-center justify-center bg-gray-900 ">
+      <h1 class="text-4xl font-semi-bold text-white tracking-wide">Enfocado</h1>
+    </div>
+  );
+}
